cd6ad8c93911fe31c2fd15b55fe0e5391cd62418
Goldrelia <aurelia.bouliane@gmail.com>
2025-01-22 21:36:38 -0500

    select challenge

---
+++ SELECT_challenge.sql
@@ -0,0 +1,4 @@
+-- Situation: We want to send out a promotional email to our existing customers!
+-- Use a SELECT statement to grab the first and last names of every customer and their email address
+
+SELECT first_name, last_name, email FROM customer;
