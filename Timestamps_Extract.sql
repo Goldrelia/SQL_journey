a4b87881f442915fb8c3299eec35013cbfb1e61c
Goldrelia <aurelia.bouliane@gmail.com>
2025-02-07 17:43:49 -0500

    time stamp stuff done

---
+++ Timestamps_Extract.sql
@@ -0,0 +1,9 @@
+-- During which months did payments occur?
+-- Format your answer to return back the full month name
+SELECT DISTINCT(TO_CHAR(payment_date, 'MONTH')) FROM payment
+
+-- During which months did payments occur?
+-- Format your answer to return back the full month name
+-- DOW -> get day of week and return as integer
+SELECT COUNT(*) FROM payment
+WHERE EXTRACT(DOW FROM payment_date) = 1; 
