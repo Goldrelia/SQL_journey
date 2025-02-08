fb2067e2a8dfdb5b1eb454f1cd640c2997c6b34a
Goldrelia <aurelia.bouliane@gmail.com>
2025-02-08 12:54:54 -0500

    corrected mistakes

--- exercises_db/Assessment_Test_2.sql
+++ exercises_db/Assessment_Test_2.sql
@@ -1,3 +1,5 @@
+-- add cd. since under the cd schema
+
 -- How can you retrieve all the information from the cd.facilities table?
 SELECT * FROM cd.facilities;
 
@@ -7,12 +9,12 @@ SELECT name, membercost FROM cd.facilities;
 
 -- How can you produce a list of facilities that charge a fee to members?
 SELECT * FROM cd.facilities
-WHERE membercost != 0;
+WHERE membercost > 0;
 
 -- How can you produce a list of facilities that charge a fee to members, and that fee is less than 1/50th of the monthly maintenance cost? 
 -- Return the facid, facility name, member cost, and monthly maintenance of the facilities in question.
 SELECT facid, name, membercost, monthlymaintenance FROM cd.facilities
-WHERE membercost < (1.0/50) * monthlymaintenance AND membercost != 0;
+WHERE membercost < (1/50.0) * monthlymaintenance AND membercost > 0;
 
 -- How can you produce a list of all facilities with the word 'Tennis' in their name?
 SELECT * FROM cd.facilities
@@ -20,9 +22,10 @@ WHERE name LIKE '%Tennis%';
 
 -- How can you retrieve the details of facilities with ID 1 and 5? Try to do it without using the OR operator.
 SELECT * FROM cd.facilities
-WHERE facid in (1, 5);
+WHERE facid IN (1, 5);
 
 -- How can you produce a list of members who joined after the start of September 2012? 
 -- Return the memid, surname, firstname, and joindate of the members in question.
 SELECT memid, surname, firstname, joindate FROM cd.members
-WHERE joindate > '2012-09-01';
+WHERE joindate >= '2012-09-01';
+
